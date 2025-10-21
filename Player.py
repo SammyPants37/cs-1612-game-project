807aecc3552f97ec8b4b34492fb082a5b81f6d7f
Riley Ronning <rilesronn@gmail.com>
2025-10-21 15:01:26 -0500

    Implemented simplifications of grab_item()

--- Player.py
+++ Player.py
@@ -23,21 +23,15 @@ class Player:
 
     def grab_item(self, new_item: Constants.Items):
         if new_item.value != 0: #if you aren't trying to grab nothing
-            for i in range(Constants.ItemLimit): #iterates over all items in inventory
-                if self.items[i].value == 0: #if there is a nothing item (no item)
-                    self.items[i] = new_item #the first nothing in the list is replaced by the new item
+            for given_item in self.items: #iterates over all items in inventory
+                if given_item.value == 0: #if the item you are on is nothing
+                    self.items[self.items.index(given_item)] = new_item # 1st nothing in the list is replaced by the new item
                     print(f"{new_item.name} grabbed")
                     return Constants.Items.nothing #returns nothing to be dropped on the tile
-                elif i == (Constants.ItemLimit - 1): #elif there are no nothing items
-                    for j in range(Constants.ItemLimit): #iterates over items in inventory again
-                        if self.items[j] != new_item: #finds if there is an item different from the grabbed one
-                            dropped_item = self.items[j] # dropped item is defined as this different item
-                            self.items[j] = new_item #the first different item in the list is replaced by the new item
-                            print(f"{new_item.name} grabbed, {dropped_item.name} dropped")
-                            return dropped_item
-                        elif j == (Constants.ItemLimit - 1): #elif a different item is not found
-                            print(f"{new_item.name} switched with {new_item.name}")
-                            return new_item #drops the item grabbed
-        else:
-            print(f"{new_item.name} grabbed") #nothing is grabbed
-            return new_item
+            for given_item in self.items: #iterates over items in inventory again
+                if given_item != new_item: #finds if there is an item different from the grabbed one
+                    self.items[self.items.index(given_item)] = new_item # 1st different item in the list is replaced by the new item
+                    print(f"{new_item.name} grabbed, {given_item.name} dropped")
+                    return given_item #returns the different item to be dropped, not overwritten when list changed since local
+        print(f"{new_item.name} grabbed, {new_item.name} dropped") #nothing is grabbed or dropped (or same thing is)
+        return new_item
