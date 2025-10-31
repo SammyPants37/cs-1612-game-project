0bab1db433b58dfcb7e7eb05289aee1e1b81dbf8
Riley Ronning <rilesronn@gmail.com>
2025-10-31 12:55:26 -0500

    autoinspect, fixed \n for last QLF

--- Constants.py
+++ Constants.py
@@ -12,16 +12,16 @@ class Minerals:
     class mineralTypes(Enum):
         unminable = (0, "Worthless Rock", 0, "Congratulations! You mined a worthless rock!")
         diamond = (1, "Diamond", 100000, "Your eyes glimmer at the sight of a room laden with diamonds. You stuff your bag full of them until your hands bleed.")
-        ruby = (2, "Ruby", 85000, "Your lantern lights up a crimson hue in a crevice to your side. Preparing for the worst, you investigate,\n leading your bag to become loaded with rubies.")
-        emerald = (3, "Emerald", 65000, "The green shine of emeralds flash across your eyes as you cross a corner. Investigating leads to a sizable pocket,\n and a sizable profit, in your bag.")
-        sapphire = (4, "Sapphire", 35000, "After passing by a small stream several times you realize the blue hue shining back at you is a gemstone!\n You gather a good haul of sapphire, at least it’ll sell for a fortune.")
+        ruby = (2, "Ruby", 85000, "Your lantern lights up a crimson hue in a crevice to your side. Preparing for the worst, you investigate,\nleading your bag to become loaded with rubies.")
+        emerald = (3, "Emerald", 65000, "The green shine of emeralds flash across your eyes as you cross a corner. Investigating leads to a sizable pocket,\nand a sizable profit, in your bag.")
+        sapphire = (4, "Sapphire", 35000, "After passing by a small stream several times you realize the blue hue shining back at you is a gemstone!\nYou gather a good haul of sapphire, at least it’ll sell for a fortune.")
         amethyst = (5, "Amethyst", 10000, "You sing “Purple rock, purple rock, gotta love purple rocks” as you mine numerous geodes of amethysts.")
         platinum = (6, "Platinum", 5500, "You feel relieved that your bag nullifies the weight of minerals, you lug a ton of platinum into your bag.")
-        gold = (7, "Gold", 3500, "You’ve heard it works pretty good as a replacement for teeth, maybe that’s why humans find it so valuable…\n you collect a modest amount of gold hoping it’ll be worth something.")
-        silver = (8, "Silver", 2500, "All the fancy dwarfs used this for cutlery, you hope it sells the same to fancy humans.\n You store a heap of silver in your bag.")
-        iron = (9, "Iron", 1750, "As a dwarf you can never complain when there’s iron involved, or at least that’s what all the village smiths said.\n You mine a large vein of iron that’d make your ancestors proud.")
+        gold = (7, "Gold", 3500, "You’ve heard it works pretty good as a replacement for teeth, maybe that’s why humans find it so valuable…\nyou collect a modest amount of gold hoping it’ll be worth something.")
+        silver = (8, "Silver", 2500, "All the fancy dwarfs used this for cutlery, you hope it sells the same to fancy humans.\nYou store a heap of silver in your bag.")
+        iron = (9, "Iron", 1750, "As a dwarf you can never complain when there’s iron involved, or at least that’s what all the village smiths said.\nYou mine a large vein of iron that’d make your ancestors proud.")
         copper = (10, "Copper", 750, "Not as good as iron, but not the worst in the world for forging. You fill your bag with wads of copper.")
-        coal = (11, "Coal", 250, "Your eyes glimmer at the sight of a room laden with di–coal. Just coal. With tears in your eyes from the wasted time,\n you begrudgingly stuff your bag with clumps of coal.")
+        coal = (11, "Coal", 250, "Your eyes glimmer at the sight of a room laden with di–coal. Just coal. With tears in your eyes from the wasted time,\nyou begrudgingly stuff your bag with clumps of coal.")
         monsterDen = (12, "Maulwurf Egg", 6000, "you hack away at the maulwurf den and are surprised to find that you receive a maulwurf egg for your troubles.")
 
         # expanding on an Enum with a __init__ allows more than value and name to be associated with a variable
